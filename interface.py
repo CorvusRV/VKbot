d202a7d289feec774e946ab941b925af8c4e4840
CorvusRV <119522101+CorvusRV@users.noreply.github.com>
2023-06-05 21:52:55 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -34,25 +34,3 @@ class BotInterface():
 if __name__ == '__main__':
     bot = BotInterface(comunity_token)
     bot.event_handler()
-# bh = vk_api.VkApi(token=comunity_token)
-# give = bh.get_api()
-# longpoll = VkLongPoll(bh)
-#
-#
-# def blasthack(id, text):
-#     bh.method('messages.send', {'user_id' : id, 'message' : text, 'random_id': 0})
-#
-# for event in longpoll.listen():
-#     if event.type == VkEventType.MESSAGE_NEW:
-#         if event.to_me:
-#             message = event.text.lower()
-#             id = event.user_id
-#
-#             if message == 'привет':
-#                 blasthack(id, 'Привет, я бот!')
-#
-#             elif message == 'как дела?':
-#                 blasthack(id, 'Хорошо, а твои как?')
-#
-#             else:
-#                 blasthack(id, 'Я вас не понимаю! :(')
