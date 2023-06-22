0159d0194e8f6202612dca6e4a9a885af54cae7f
CorvusRV <corvusrv@gmail.com>
2023-06-22 19:59:35 +0300

    update search

--- core.py
+++ core.py
@@ -11,14 +11,13 @@ class VkTools:
     def get_profile_info(self, user_id):
 
         info, = self.api.method('users.get', {'user_id': user_id,
-                                             'fields': 'city,bdate,sex,relation,home_town'}
-                               )
+                                             'fields': 'city,bdate,sex,relation,home_town'})
         user_info = {'name': f"{info['first_name']} {info['last_name']}",
                      'id': info['id'],
                      'bdate': info.get('bdate'),
                      'sex': info.get('sex'),
-                     'city_name': info['city']['title'],
-                     'city_id': info['city']['id'],
+                     'city_name': info.get('city')['title'],
+                     'city_id': info.get('city')['id'],
                      }
         return user_info
 
@@ -27,17 +26,19 @@ class VkTools:
         формирование условий поиска
         """
         age = datetime.now().year - int(params['bdate'].split('.')[2])  # исправить, так как будет выдавать ошибку
-        search_terms = {'count': 1000,
-                        'offset': 0,
+        search_terms = {'count': 50,
+                        #'offset': 0, #offset,
                         'age_from': age - 5,
                         'age_to': age + 5,
                         'sex': 1 if params['sex'] == 2 else 2,
                         'city': params['city_id'],
+                        'city_name': params['city_name'],
                         'status': 6,
                         'is_closed': False}
         return search_terms
 
-    def serch_users(self, search_terms):
+    def search_users(self, search_terms, offset):
+        search_terms['offset'] = offset
         users = self.api.method('users.search', search_terms)
         return users['items'] if users.get('items') is not None else []
 
@@ -46,9 +47,8 @@ class VkTools:
         получение фотографий пользователя из профеля
         """
         photos = sorted(photos, key=lambda x: x['likes']['count']+x['comments']['count'], reverse=True)[0:3]
-        attachment = [photo['sizes'][-1]['url'] for photo in photos]
-        attachment = []  # для тестов
-        return ', '.join(attachment)
+        attachment = [f'photo{photo["owner_id"]}_{photo["id"]}' for photo in photos]
+        return ','.join(attachment)
 
     def enumeration_found_users(self, users):
         user = users.pop()
@@ -56,17 +56,17 @@ class VkTools:
 
     def data_acquisition_user(self, user):
         if user['can_access_closed']:
-            get_photos = self.api.method('photos.get',
+            photos = self.api.method('photos.get',
                                      {'owner_id': user['id'],
                                       'album_id': 'profile',
                                       'extended': 1
                                       }
                                      )
-            if get_photos['count'] > 0:
-                user_name = f"{user['first_name']} {user['last_name']}"
-                attachment = self.get_photos(get_photos['items'])
-                print(user_name, attachment)
-                return user['id'], user_name, attachment
+            if photos['count'] > 0:
+                photos = sorted(photos['items'], key=lambda x: x['likes']['count'] + x['comments']['count'],
+                                reverse=True)[0:3]
+                attachment = [f'photo{photo["owner_id"]}_{photo["id"]}' for photo in photos]
+                return user['id'], f"{user['first_name']} {user['last_name']}", ','.join(attachment)
         return user['id'], None, None
 
     def city_id(self, name):
@@ -76,9 +76,8 @@ class VkTools:
                                 'need_all': 0})
         if city['count'] == 0:
             return None, None
-        print(city)
         if len(name) > 1:
             for c in city['items']:
                 if c.get('region') == name[1].capitalize():
-                    return name[0], c['id']
-        return name[0], city['items'][0]['id']
+                    return c['id'], name[0]
+        return city['items'][0]['id'], name[0]
--- interface.py
+++ interface.py
@@ -1,6 +1,7 @@
 import vk_api
 import re
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
 
 from config import comunity_token, acces_token
 from core import VkTools
@@ -13,21 +14,26 @@ class BotInterface:
         self.interface = vk_api.VkApi(token=comunity_token)
         self.api = VkTools(acces_token)
         self.db_logic = DBLogic()  # управление базой данных
+        self.longpoll = VkLongPoll(self.interface)
         self.params = None
         self.search_terms = None
+        self.offset = 0
 
-    def message_send(self, user_id, text):
+    def message_send(self, user_id, text, attachment=None):
         self.interface.method('messages.send',
                               {'user_id': user_id,
                                'message': text,
-                               'random_id': 0}
+                               'attachment': attachment,
+                               'random_id': get_random_id()}
                               )
 
     def request_for_data_user(self):  # переделать цикл
-        users = self.api.serch_users(self.search_terms)
         user_name = None
+        print(self.offset)
+        users = self.api.search_users(self.search_terms, self.offset)
         while user_name is None:
             user = self.api.enumeration_found_users(users)
+            self.offset += 1
             if not self.db_logic.getting_verified_id(self.params['id'], user['id']):
                 user_id, user_name, attachment = self.api.data_acquisition_user(user)
                 self.db_logic.viewing(self.params['id'], user_id)
@@ -42,32 +48,28 @@ class BotInterface:
         return '\n'.join(list_liked_users)
 
     def event_handler(self):
-        longpoll = VkLongPoll(self.interface)
-
-        for event in longpoll.listen():
+        for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 command = event.text.lower()
-
                 if command in ('привет', 'старт'):  # стартовая команда
                     if self.params is None:
                         self.params = self.api.get_profile_info(event.user_id)
-                    self.message_send(event.user_id, f'Здравствуй {self.params["name"]}')
+                    self.message_send(event.user_id, f'Здравствуте {self.params["name"]}')
+
                     # проверка данных пользователя
                     if self.params['bdate'] is None:
-                        self.message_send(event.user_id, 'Введите дату рождения в формате ДД.ММ.ГГГГ')
-                        for event in longpoll.listen():
+                        self.message_send(event.user_id, 'Введите дату рождения ДД.ММ.ГГГГ')
+                        for event in self.longpoll.listen():
                             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                                command = event.text.lower()
-                                if re.search(r'\d\d.\d\d.\d{4}', command):
-                                    bdate = command
-                                    if bdate == '11.11.1111':
-                                        self.params['dbase'] = bdate
-                                        self.message_send(event.user_id, 'Дата рождения принята')
-                                        break
+                                bdate = event.text.lower()
+                                if re.search(r'\d\d.\d\d.\d{4}', bdate):
+                                    self.params['dbase'] = bdate
+                                    self.message_send(event.user_id, 'Дата рождения принята')
+                                    break
                                 self.message_send(event.user_id, 'Попробуйте еще раз')
                     if self.params['sex'] is None:
                         self.message_send(event.user_id, 'Введите свой пол м/ж')
-                        for event in longpoll.listen():
+                        for event in self.longpoll.listen():
                             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                                 sex = event.text.lower()
                                 if sex == 'м' or sex == 'ж':
@@ -75,19 +77,20 @@ class BotInterface:
                                     self.message_send(event.user_id, 'Пол принят')
                                     break
                                 self.message_send(event.user_id, 'Попробуйте еще раз')
-                    if self.params['city'] is None:
+                    if self.params['city_id'] is None:
                         self.message_send(event.user_id, 'Введите название вашего города проживания.\n'
                                                          'Через занятую можно уточнить область.')
-                        for event in longpoll.listen():
+                        for event in self.longpoll.listen():
                             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                                 city = event.text.lower()
-                                city_name, city_id = self.api.city_id(city)
+                                city_id, city_name = self.api.city_id(city)
                                 if city_id is not None:
                                     self.params['city_id'] = city_id
                                     self.params['city_name'] = city_name.capitalize()
                                     self.message_send(event.user_id, f"Город {self.params['city_name']} принят")
                                     break
                                 self.message_send(event.user_id, 'Попробуйте еще раз')
+
                 # команды для проверки личных данных
                 elif command == 'мои данные' and self.params is not None:  # проверка данных клиента
                     self.message_send(event.user_id, f"name: {self.params['name']}\n"
@@ -101,28 +104,32 @@ class BotInterface:
                         self.search_terms = self.api.formation_search_terms(self.params)
                     self.message_send(event.user_id, f"age: {self.search_terms.get('age_from')} - {self.search_terms.get('age_to')}\n"
                                                      f"sex: {self.search_terms.get('sex')}\n"
-                                                     f"city_id: {self.search_terms.get('city')}")
+                                                     f"city: {self.search_terms.get('city_name')}")
                 # команды для изменнений условий поиска
                 elif re.search(r'поиск по возрасту - \d{2,3}-\d{2,3}$', command) and self.search_terms is not None:
                     age = command.split(' - ')
                     age_from, age_to = age[1].split('-')
-                    self.search_terms['age_from'] = int(age_from)
-                    self.search_terms['age_to'] = int(age_to)
+                    self.search_terms['age_from'], self.search_terms['age_to'] = int(age_from), int(age_to)
                     self.message_send(event.user_id, f"age: {self.search_terms.get('age_from')} - {self.search_terms.get('age_to')}")
                 elif re.search(r'поиск по полу - [12]{1}', command) and self.search_terms is not None:  # воспринимает 1,2,12,21 исправить
                     sex = command.split()[4]
                     self.search_terms['sex'] = sex
                     self.message_send(event.user_id, f"sex: {self.search_terms.get('sex')}")
                 elif re.search(r'поиск по городу - \w', command) and self.search_terms is not None:
-                    name = command.split(' ', 4)[4]
-                    name, self.search_terms['city'] = self.api.city_id(name)
-                    self.message_send(event.user_id, f"city_id: {self.search_terms.get('city')}")
+                    city = command.split(' - ')[1]
+                    city_id, city_name = self.api.city_id(city)
+                    if city_id is not None:
+                        self.search_terms['city_id'] = city_id
+                        self.search_terms['city_name'] = city_name.capitalize()
+                        self.message_send(event.user_id, f"city_id: {self.search_terms['city_name']}")
+                    else:
+                        self.message_send(event.user_id, "Попробуй еще раз")
                 elif command == 'поиск' and self.params is not None:
                     if self.search_terms is None:
                         self.search_terms = self.api.formation_search_terms(self.params)
                     user_id, user_name, attachment = self.request_for_data_user()
-                    self.message_send(event.user_id,
-                                      f"Встречайте {user_name}\nhttps://vk.com/id{user_id}\n\n{attachment}")
+                    self.message_send(event.user_id, f'Встречайте {user_name}'
+                                                     f'\nhttps://vk.com/id{user_id}', attachment=attachment)
                 elif command == 'нравится' and self.params is not None:
                     self.db_logic.mark_user_like(self.params['id'], user_id)
                     self.message_send(event.user_id, f"{user_id}")
